dea9d7bb1b8dacc5755eaf7b066434bcf8a0d017
MahmoudRizk <mahmoudahmedrizk@gmail.com>
2019-05-18 00:35:00 +0200

    Adding campaign app

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,6 +80,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     "cognitive.users.apps.UsersConfig",
+    "cognitive.campaign.apps.CampaignConfig",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
